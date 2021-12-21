911112c52299d3af818fd10e7ae832e121d61825
Your Name <you@example.com>
2021-12-21 16:01:55 +0800

    book

---
+++ book.java
@@ -0,0 +1,10 @@
+public class book {
+    int id;
+    String name;
+    String author;
+
+    @Override
+    public String toString() {
+        return "book [author=" + author + ", id=" + id + ", name=" + name + "]";
+    }
+}
